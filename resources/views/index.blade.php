23b5b948d615dfbaf06cffcd2e978b861edd4111
rexit7771 <pramaskoro@cmail.com>
2023-06-16 15:29:24 +0700

    hcm-app

---
+++ resources/views/index.blade.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HCM Applications</title>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,600,500,300,700' rel='stylesheet' type='text/css'>
+    <link rel="icon" href="{{ asset('img/logo/favicon.ico') }}" type="image/x-icon"/>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/templatemo_main.css')}}">
+    <script src="https://kit.fontawesome.com/bf2986434e.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div id="main-wrapper">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center templatemo-logo margin-top-20">
+            <h1 class="templatemo-site-title">
+                <a href="#"><b>Human Capital Management Web Applications</b></a>
+            </h1>
+        </div>
+
+        <div class="image-section">
+            <div class="image-container">
+                <img src="images/zoom-bg-1.jpg" id="menu-img" class="main-img inactive" alt="Zoom HTML5 Template by templatemo.com">
+                <img src="images/zoom-bg-2.jpg" id="products-img" class="inactive" alt="Product">
+                <img src="images/zoom-bg-3.jpg" id="services-img"  class="inactive" alt="Services">
+                <img src="images/zoom-bg-4.jpg" id="about-img" class="inactive" alt="About">
+                <img src="images/zoom-bg-5.jpg" id="contact-img" class="inactive" alt="Contact">
+                <img src="images/zoom-bg-6.jpg" id="company-intro-img" class="main-img inactive" alt="Company Intro">
+                <img src="images/zoom-bg-7.jpg" id="testimonials-img" class="main-img inactive" alt="Testimonials">
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2">
+                <div class="templatemo-content">
+                    <section id="menu-section" class="active">
+                        <div class="row">
+                            <div class="col-lg-4 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://hrms.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fa-solid fa-people-group fa-7x"></i> --}}
+                                            <img src="{{asset('/images/hrms.png')}}" alt="HRMS NU">
+                                            <h2>HRMS NU</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://hrms-snu.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fa-solid fa-users-line fa-7x"></i> --}}
+                                            <img src="{{asset('/images/hrmsSNU.png')}}" alt="HRMS SNU">
+                                            <h2>HRMS SNU</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://hrms-nps.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fa-solid fa-people-line fa-7x"></i> --}}
+                                            <img src="{{asset('/images/hrmsNPS.png')}}" alt="HRMS NPS">
+                                            <h2>HRMS NPS</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-4 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://elearning.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fas fa-book fa-7x"></i> --}}
+                                            <img src="{{asset('/images/elearning.png')}}" alt="E-Learning">
+                                            <h2>E-Learning</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://job-portal.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fa-solid fa-handshake fa-7x"></i> --}}
+                                            <img src="{{asset('/images/jobPortal.png')}}" alt="Job Portal">
+                                            <h2>Job Portal</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://dashhcm.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fa-solid fa-chart-column fa-7x"></i> --}}
+                                            <img src="{{asset('/images/dashboard.png')}}" alt="Dashboard HCM">
+                                            <h2>Dashboard HCM</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-3 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://audit-hcm.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fa-solid fa-person-chalkboard fa-4x"></i> --}}
+                                            <img src="{{asset('/images/audit.png')}}" alt="Safety Voice">
+                                            <h2>Audit</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://safetyvoice.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fas fa-hard-hat fa-4x"></i> --}}
+                                            <img src="{{asset('/images/safetyVoice.png')}}" alt="Safet Voice">
+                                            <h2>Safety Voice</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://pettycash.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fas fa-rupiah-sign fa-4x"></i> --}}
+                                            <img src="{{asset('/images/pettyCash.png')}}" alt="Petty Cash">
+                                            <h2>Petty Cash</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 margin-bottom-20">
+                                <div class="col">
+                                    <a href="https://cuticrew.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fas fa-calendar-alt fa-4x"></i> --}}
+                                            <img src="{{asset('/images/cutiCrew.png')}}" alt="Cuti Crew">
+                                            <h2>Cuti Crew</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-3 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://frmusul.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fas fa-file-alt fa-4x"></i> --}}
+                                            <img src="{{asset('/images/formUsul.png')}}" alt="Form Usul">
+                                            <h2>Form Usul</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-2 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://lemburcrew.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fas fa-clock fa-4x"></i> --}}
+                                            <img src="{{asset('/images/lemburCrew.png')}}" alt="Lembur Crew">
+                                            <h2>Lembur Crew</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-2 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://fpkcrew.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fas fa-angle-double-up fa-4x"></i> --}}
+                                            <img src="{{asset('/images/FPKCrew.png')}}" alt="FPK Crew">
+                                            <h2>FPK Crew</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-2 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://notulensi.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fas fa-angle-double-up fa-4x"></i> --}}
+                                            <img src="{{asset('/images/notulensi.png')}}" alt="Invoice">
+                                            <h2>Notulensi</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 margin-bottom-20 big-box">
+                                <div class="col">
+                                    <a href="https://invoice.niramasutama.com" target="_blank" >
+                                        <div class="black-bg btn-menu">
+                                            {{-- <i class="fas fa-angle-double-up fa-4x"></i> --}}
+                                            <img src="{{asset('/images/Invoice.png')}}" alt="Invoice">
+                                            <h2>Monitoring Invoice</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer">
+                <p class="footer-text">Copyright &copy; 2023 HCM PT.Niramas Utama</p>
+            </div>
+        </div>
+    </div>
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('js/jquery-ui.min.js')}}"></script>
+    <script src="{{asset('js/jquery.backstretch.min.js')}}"></script>
+    <script src="{{asset('js/templatemo_script.js')}}"></script>
+</body>
+</html>
